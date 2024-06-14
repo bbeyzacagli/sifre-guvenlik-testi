77bc5e56694b66ba6aeb3d605da138b9e94a09ee
bbeyzacagli <beyzacagli2@gmail.com>
2024-06-14 22:00:02 +0300

    .

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿/********************************************************************************************************************
+**				                                                           SAKARYA ÜNİVERSİTESİ
+**				                                                BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				                                                      BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ        
+**				                                                     NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					                                                      2021-2022 BAHAR DÖNEMİ
+**	
+**				                                            ÖDEV NUMARASI.......: 1
+**				                                            ÖĞRENCİ ADI.........: BEYZA ÇAĞLI
+**				                                            ÖĞRENCİ NUMARASI....: B211210084
+**                                                          DERSİN ALINDIĞI GRUP: 1A
+*
+********************************************************************************************************************/
+
+
+
+using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            while (metotlar.puan < 70)                         //sifrenin puani 70'ten kucuk olursa sifreyi yeniden almak icin while dongusu kullandim.
+            {
+                string sifre;                                  
+                Console.Write("Sifrenizi giriniz: ");
+                sifre = Console.ReadLine();
+                Console.WriteLine();
+                string bosluk = " ";                           //sifrede bosluk olup olmadigini kontrol edebilmek icin bir bosluk metni tanimladim.
+                char[] rakamdizisi = new char[sifre.Length];        
+                rakamdizisi = sifre.ToCharArray();             //sifredeki buyuk ve kucuk harfleri, rakamlari ve sembolleri kontrol edebilmek icin sifreyi karakter dizisine cevirdim.
+                metotlar.BuyukHarfSayisi(sifre);               //asagidaki while dongusunun kontrolunde ihtiyacim olan buyuk ve kucuk harfleri, rakamlari ve sembolleri kullanabilmek icin metotlar sinifindan gerekli metotlari cagirdim.
+                metotlar.KucukHarfSayisi(sifre);
+                metotlar.SembolSayisi(sifre);
+                metotlar.RakamSayisi(sifre);
+
+                while (sifre.Length < 9 || sifre.Length > 10 || sifre.Contains(bosluk) == true || metotlar.buyukHarf == 0 || metotlar.kucukHarf == 0 || metotlar.rakam == 0 || metotlar.sembol == 0)    
+                    //sifrede olmamasi gereken sartlari yazdim. bir tanesi bile sifrede saglanirsa sifre hatali olacak ve dogru sifre girilene kadar yeni sifre alinacak.
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Hatali sifre girdiniz. Tekrar deneyiniz.");
+                    Console.WriteLine();
+                    metotlar.sifirla();
+                    Console.Write("Sifrenizi giriniz: ");
+                    sifre = Console.ReadLine();
+                    Console.WriteLine();
+                    metotlar.BuyukHarfSayisi(sifre);
+                    metotlar.KucukHarfSayisi(sifre);
+                    metotlar.SembolSayisi(sifre);
+                    metotlar.RakamSayisi(sifre);
+                }
+
+                if (sifre.Length == 9)                                          //sifre 9 karakterli ise sifre puanina 10 puan ekledim.
+                    metotlar.puan += 10;
+
+                Console.WriteLine("Sifrenin puani: " + metotlar.puan);      
+
+                if (metotlar.puan > 70 && metotlar.puan < 90)                  //sifrenin puani 70 ile 90 arasindaysa sifreyi kabul ettim.
+                { 
+                    Console.WriteLine("Sifre kabul edildi.");
+                    break;                                                      //donguden cikabilmek icin break kullandim.
+                }
+                
+                if (metotlar.puan >= 90 && metotlar.puan <= 100)                //sifrenin puani 90 ile 100 arasinda ise sifreyi kabul ettim.
+                { 
+                    Console.WriteLine("Guclu sifre, sifre kabul edildi.");
+                    break;                                                      //donguden cikabilmek icin break kullandim.
+                }
+                
+                if (metotlar.puan <= 70)                                         //sifrenin puani 70'ten kucukse sifre zayif oldugu icin yeniden sifre istedim.
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Sifre zayif. Tekrar deneyiniz.");
+                    Console.WriteLine();
+                }
+
+                metotlar.sifirla();                                             //tekrar sifre girilmesi durumunda puani, buyuk harfi, kucuk harfi, rakami ve sembolu sifirlamak icin sifirla metodunu cagirdim.
+            }
+
+            Console.ReadKey();                                                  //ekranin kapanmamasi icin
+        }
+    }
+}
+
---
+++ metotlar.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev
+{
+    public static class metotlar
+    {
+        public static int buyukHarf = 0;        //buyuk harf sayisini icinde tutacak degisken
+        public static int kucukHarf = 0;        //kucuk harf sayisini icinde tutacak degisken
+        public static int rakam = 0;            //rakam sayisini icinde tutacak degisken
+        public static int sembol = 0;           //sembol sayisini icinde tutacak degisken
+        public static int puan = 0;             //sifrenin puanini icinde tutacak degisken
+
+        public static void sifirla()                         //yeniden sifre girilmesi durumunda eski verileri sifirlamak icin bir metod olusturdum.
+        {
+            buyukHarf = 0;
+            kucukHarf = 0;
+            rakam = 0;
+            sembol = 0;
+            puan = 0;
+        }
+
+        public static void BuyukHarfSayisi(string sifre)    //buyuk harf sayisini bulmak icin bir metod olusturdum.
+        {
+            char[] dizi = new char[sifre.Length];
+            dizi = sifre.ToCharArray();                     //girilen sifrede buyuk harf olup olmadigini kontrol edebilmek icin sifreyi karakter dizisine cevirdim.
+
+            for (int i = 0; i < sifre.Length; i++)          //karakter dizisinde buyuk harf var mi diye kontrol edebilmek icin 0'dan baslayan ve sifrenin uzunluguna kadar sirayla giden bir for dongusu olusturdum.
+            {
+                bool sonuc = Char.IsUpper(dizi[i]);         //buyuk harf olup olmadigini kontrol eden metodu kullandim ve sonucu bir bool degiskenine atadim.
+
+                if (sonuc == true)                          //sonuc degiskenine atadigim deger eger dogruysa yani sifrede buyuk harf varsa buyuk harf sayisini icinde tutan buyukHarf degiskenini 1 arttir.
+                    buyukHarf++;                
+            }
+
+            if (buyukHarf == 1)                             //eger buyuk harf sayisi 1 ise sifre puanina 10 ekle, 2 ve 2'den fazla ise 20 puan ekle.
+                puan += 10;
+            if (buyukHarf >= 2)
+                puan += 20;
+
+            Console.WriteLine("Sifredeki buyuk harf sayisi: " + buyukHarf);     
+        }
+
+        public static void KucukHarfSayisi(string sifre)        //kucuk harf sayisini bulmak icin bir metod olusturdum.
+        {
+            char[] dizi = new char[sifre.Length];
+            dizi = sifre.ToCharArray();                         //girilen sifrede kucuk harf olup olmadigini kontrol edebilmek icin sifreyi karakter dizisine cevirdim.
+
+            for (int i = 0; i < sifre.Length; i++)              //karakter dizisinde kucuk harf var mi diye kontrol edebilmek icin 0'dan baslayan ve sifrenin uzunluguna kadar sirayla giden bir for dongusu yaptim.
+            {
+                bool sonuc = Char.IsLower(dizi[i]);             //kucuk harf olup olmadigini kontrol eden metodu kullandim ve sonucu bir bool degiskenine atadim.
+
+                if (sonuc == true)                              //eger sifrede kucuk harf varsa kucuk harf sayisini icinde tutan kucukHarf degiskenini 1 arttir.
+                    kucukHarf++;                
+            }
+
+            if (kucukHarf == 1)                                 //eger kucuk harf sayisi 1 ise sifre puanina 10 ekle, 2 ve 2'den fazla ise 20 puan ekle.
+                puan += 10;
+            if (kucukHarf >= 2)
+                puan += 20;
+
+            Console.WriteLine("Sifredeki kucuk harf sayisi: " + kucukHarf);   
+        }
+
+        public static void RakamSayisi(string sifre)    //sifredeki rakam sayisini bulmak icin bir metod olusturdum.
+        {
+            char[] dizi = new char[sifre.Length];
+            dizi = sifre.ToCharArray();                 //girilen sifrede rakam olup olmadigini kontrol edebilmek icin sifreyi karakter dizisine cevirdim.
+
+            for (int i = 0; i < sifre.Length; i++)      //karakter dizisinde rakam var mi diye kontrol edebilmek icin 0'dan baslayan ve sifrenin uzunluguna kadar sirayla giden bir for dongusu yaptim.
+            {
+                bool sonuc = Char.IsDigit(dizi[i]);     //rakam olup olmadigini kontrol eden metodu kullandim ve sonucu bir bool degiskenine atadim.
+
+                if (sonuc == true)                      //eger sifrede rakam varsa rakam sayisini icinde tutan rakam degiskenini 1 arttir.
+                    rakam++;              
+            }
+
+            if (rakam == 1)                             // eger rakam sayisi 1 ise sifre puanina 10 ekle, 2 ve 2'den fazla ise 20 puan ekle.
+                puan += 10;
+            if (rakam >= 2)
+                puan += 20;
+
+            Console.WriteLine("Sifredeki rakam sayisi: " + rakam);      
+        }
+
+        public static void SembolSayisi(string sifre)                    //sifredeki sembol sayisini bulmak icin bir metod olusturdum.
+        {
+            char[] dizi = new char[sifre.Length];
+            dizi = sifre.ToCharArray();                                  //girilen sifrede sembol olup olmadigini kontrol edebilmek icin sifreyi karakter dizisine cevirdim.
+
+            for (int i = 0; i < sifre.Length; i++)                       //karakter dizisinde sembol var mi diye kontrol edebilmek icin 0'dan baslayan ve sifrenin uzunluguna kadar sirayla giden bir for dongusu yaptim.
+            {
+                bool sonuc = Char.IsPunctuation(dizi[i]);                //noktalama isaretleri olup olmadigini kontrol eden metodu kullandim ve sonucu sonuc degiskenine atadim.
+                bool sonuc2 = Char.IsSymbol(dizi[i]);                    //sembol olup olmadigini kontrol eden metodu kullandim ve sonucu sonuc2 degiskenine atadim.
+                if (sonuc == true || sonuc2 == true)                     //eger sifrede sembol (veya noktalama isareti) varsa sembol (ve noktalama isareti) sayisini icinde tutan sembol degiskenini 1 arttir.
+                    sembol++;
+            }
+
+            puan += sembol * 10;                                         //sifredeki her sembol icin puani 10 arttir.
+            Console.WriteLine("Sifredeki sembol sayisi: " + sembol);    
+        }
+    }
+}
